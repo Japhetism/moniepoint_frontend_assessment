b40c4b8b5e65b6cb373face280f7e31b2711bbdb
Babatunde Ojo <babatundeojo30@gmail.com>
2024-05-19 22:36:25 +0100

    add animation

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react"
+import ScrollAnimation from "react-animate-on-scroll";
 import { Layout } from "../components/Layout";
 import { Header } from "../components/Header";
 import { Analytics } from "./components/Analytics";
@@ -8,13 +10,72 @@ import { GetStarted } from "./components/GetStarted";
 import { Footer } from "../components/Footer";
 
 export const Home = () => {
+    const [thereIsScroll, setThereIsScroll] = useState(false);
+
+    useEffect(() => {
+        const handleScrolling = () => {
+            const windowHeight = window.innerHeight;
+            const documentHeight = document.body.clientHeight;
+            
+            if (windowHeight === documentHeight) {
+                setThereIsScroll(false);
+            } else {
+                setThereIsScroll(true);
+            }
+        }
+
+        handleScrolling();
+        window.addEventListener("scroll", handleScrolling);
+
+        return () => {
+            window.addEventListener("scroll", handleScrolling);
+        }
+    }, []);
+
+    console.log("there is scroll ", thereIsScroll);
     return <Layout>
         <Header />
-        <Analytics />
-        <Report />
-        <TrafficSources />
-        <Idea />
-        <GetStarted />
-        <Footer />
+        {thereIsScroll ? (
+            <ScrollAnimation animateIn="fadeInDown">
+                <Analytics />
+            </ScrollAnimation>
+        ) : (
+            <Analytics />
+        )}
+        {thereIsScroll ? (
+            <ScrollAnimation animateIn="fadeInDown">
+                <Report />
+            </ScrollAnimation>
+        ) : (
+            <Report />
+        )}
+        {thereIsScroll ? (
+            <ScrollAnimation animateIn="fadeInDown">
+                <TrafficSources />
+            </ScrollAnimation>
+        ) : (
+            <TrafficSources />
+        )}
+        {thereIsScroll ? (
+            <ScrollAnimation animateIn="fadeInDown">
+                <Idea />
+            </ScrollAnimation>
+        ) : (
+            <Idea />
+        )}
+        {thereIsScroll ? (
+            <ScrollAnimation animateIn="fadeInDown">
+                <GetStarted />
+            </ScrollAnimation>
+        ) : (
+            <GetStarted />
+        )}
+        {thereIsScroll ? (
+            <ScrollAnimation animateIn="fadeInDown">
+                <Footer />
+            </ScrollAnimation>
+        ) : (
+            <Footer />
+        )}    
     </Layout>
 }
