9569457209a0cd6b8ef9ebe384d2f97de16b38ea
Babatunde Ojo <babatundeojo30@gmail.com>
2024-05-19 15:17:37 +0100

    revamp header

--- src/pages/components/Header/index.tsx
+++ src/pages/components/Header/index.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
     return (
-        <div className="flex flex-row justify-between bg-black text-white m-5 p-2 rounded-2xl">
+        <div className="flex flex-col md:flex-row justify-between bg-black text-white m-5 p-2 rounded-2xl">
             <div className="flex flex-row pt-3 pb-3">
                 <div className="w-5">
                     <img src="/assets/images/logo.png" alt="logo" />
@@ -9,14 +9,14 @@ export const Header = () => {
                     <span className="font-extrabold">ramos</span>
                 </div>
             </div>
-            <div className="flex flex-row justify-evenly w-2/5 pt-3 pb-3 rounded-2xl backdrop-sepia-0 bg-white/15 font-thin">
+            <div className="flex flex-col md:flex-row justify-evenly space-y-6 md:space-y-0 w-2/5 pt-3 pb-3 rounded-2xl backdrop-sepia-0 md:bg-white/15 font-thin">
                 <div><span>Dashboard</span></div>
                 <div><span>Reports</span></div>
                 <div><span>Documents</span></div>
                 <div><span>History</span></div>
                 <div><span>Settings</span></div>
             </div>
-            <div className="flex flex-row justify-end">
+            <div className="flex flex-col md:flex-row justify-end mt-5 md:mt-0 pb-5 md:pb-0">
                 <button className="bg-white text-black p-3 px-10 rounded-2xl font-extralight">Sign Up</button>
             </div>
         </div>
